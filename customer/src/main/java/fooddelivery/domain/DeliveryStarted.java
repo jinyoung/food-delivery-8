0e7a14a08d69e7ddc9cf14ae6135e320f673a0e7
Jinyoung Jang <jyjang@uengine.org>
2022-08-07 16:37:05 +0900

    commit msg

---
+++ customer/src/main/java/fooddelivery/domain/DeliveryStarted.java
@@ -0,0 +1,13 @@
+package fooddelivery.domain;
+
+import fooddelivery.infra.AbstractEvent;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class DeliveryStarted extends AbstractEvent {
+
+    private Long id;
+    private String address;
+    private Long orderId;
+}
---
+++ delivery/src/main/java/fooddelivery/external/Order.java
@@ -0,0 +1,18 @@
+package fooddelivery.external;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class Order {
+
+    private Long id;
+    private String foodId;
+    private String customerId;
+    private String preference;
+    private String options;
+    private Address address;
+    private String status;
+    // keep
+
+}
